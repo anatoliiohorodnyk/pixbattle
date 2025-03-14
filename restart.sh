3ba7bb55190bc601d960f8b0e200c1bf579a4fdd
Anatolii Ohorodnyk <a.ohorodnyk@playtini.ua>
2025-03-14 14:38:26 +0000

    v24

--- restart.sh
+++ restart.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-git pull
-docker compose down --rmi server client
-docker compose up --build -d server client
+# Зупиняємо всі контейнери
+docker compose down
+
+# Видаляємо всі локальні зміни
+git reset --hard HEAD
+git clean -fd
+
+# Оновлюємо код з репозиторію
+git pull origin master
+
+# Перезапускаємо контейнери
+docker compose up --build -d
+
+# Виводимо логи
+docker compose logs -f
