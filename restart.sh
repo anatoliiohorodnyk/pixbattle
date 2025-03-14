ae34d83f4222b4041d58f8ea4713c2aee67db17d
Anatolii Ohorodnyk <a.ohorodnyk@playtini.ua>
2025-03-14 15:13:03 +0000

    v8

--- restart.sh
+++ restart.sh
@@ -3,11 +3,6 @@
 # Зупиняємо всі контейнери
 docker compose down
 
-# Видаляємо всі локальні зміни
-git reset --hard HEAD
-git clean -fd
-
-# Оновлюємо код з репозиторію
 git pull origin master
 
 # Перезапускаємо контейнери
